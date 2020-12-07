2e20cbc463928b9c0b128795b7163a862737e031
d1632 <yama.yama.1102@docomo.ne.jp>
2020-12-07 03:36:46 +0000

    mainを編集

--- main.rb
+++ main.rb
@@ -3,20 +3,33 @@ include DXOpal
 
 require_remote 'note.rb'
 
-Image.register(:note, 'images/player.png')
+# Image.register(:note, 'images/player.png')
+Image.register(:notea, 'images/note_a.png')
+Image.register(:noteb, 'images/note_b.png')
+Image.register(:notec, 'images/note_c.png')
+Image.register(:noted, 'images/note_d.png')
+
 
 
 Window.load_resources do
     Window.width  = 800
     Window.height = 600
     
-    note_img = Image[:note]
-    note_img.set_color_key([0, 0, 0])
+    # note_img = Image[:note]
+    # note_img.set_color_key([0, 0, 0])
+    notea_img = Image[:notea]
+    notea_img.set_color_key([1, 1, 1])
+    noteb_img = Image[:noteb]
+    noteb_img.set_color_key([1, 1, 1])
+    notec_img = Image[:notec]
+    notec_img.set_color_key([1, 1, 1])
+    noted_img = Image[:noted]
+    noted_img.set_color_key([1, 1, 1])
     
-    notea = Note.new(100, 0, note_img, K_V)
-    noteb = Note.new(200, 0, note_img, K_B)
-    notec = Note.new(300, 0, note_img, K_N)
-    noted = Note.new(400, 0, note_img, K_M)
+    notea = Note.new(100, 0, notea_img, K_V)
+    noteb = Note.new(200, 0, noteb_img, K_B)
+    notec = Note.new(300, 0, notec_img, K_N)
+    noted = Note.new(400, 0, noted_img, K_M)
     
     note = [notea,noteb,notec,noted]
     
